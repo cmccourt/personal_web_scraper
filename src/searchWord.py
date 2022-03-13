cb4e9b918eb9c8c543a0c7b218095f0fb2cc9656
Chris McCOurt <chrismccourt66@gmail.com>
2022-03-13 12:46:02 +0000

    Added TOTAL_KEY to store key string for common URLs. Refactored search_input function.

--- src/searchWord.py
+++ src/searchWord.py
@@ -2,21 +2,23 @@ from collections import Counter, defaultdict
 
 from src.Exceptions import DBNotAvailable
 
+TOTAL_KEY = "--total"
+
 
 def search_words_with_user_input(user_input, poodle_db):
     # User must have entered word(s) to search for
     try:
         search_result = search_input(user_input, poodle_db.index_graph.data, poodle_db.page_rank.data)
         if search_result:
-            if "Overall" in search_result:
+            if TOTAL_KEY in search_result:
                 # There has been a match with the multiple words given
-                if len(search_result["Overall"]) == 1:
+                if len(search_result[TOTAL_KEY]) == 1:
                     # Only one matching URL, so we only need to print it
                     print(f"WOOF! {user_input} was found!")
-                    print(f"{search_result['Overall']}")
+                    print(f"{search_result[TOTAL_KEY]}")
                 else:
                     # Need to organise the URLs based on their page ranking
-                    order_urls = sorted(search_result["Overall"], key=lambda x: x[1], reverse=True)
+                    order_urls = sorted(search_result[TOTAL_KEY], key=lambda x: x[1], reverse=True)
                     print(f"WOOF! {user_input} was found!")
                     for url_rank in order_urls:
                         print(f"{url_rank[0]} : {url_rank[1]}")
@@ -36,28 +38,28 @@ def search_words_with_user_input(user_input, poodle_db):
         raise DBNotAvailable
 
 
-def search_input(user_input, word_graph=None, page_rank=None):
+def search_input(user_input, index_graph: dict = None, page_rank=None):
     """Function to return list of urls found using input"""
-    if not word_graph and not page_rank:
-        return None
-    # dictionary to extract the rank for the urls found
     urls_found_rank = defaultdict(list)
+    if not index_graph and not page_rank:
+        return urls_found_rank
+    # dictionary to extract the rank for the urls found
+
     # Capture all words in user input
     process_input = [x.strip() for x in user_input.split()]
+    index_graph_search = {x.lower(): y for x, y in index_graph.items()}
     for word in process_input:
-        for key, value in word_graph.items():
-            # Check if the lowercase word matches the lowercase key (improve chances of a match)
-            if word.lower() == key.lower():
-                for url in value:
-                    # Append the url and rank to the dict
-                    urls_found_rank[word].append([url, page_rank[url]])
+        # Check if the lowercase word matches any of the lowercase keys (improve chances of a match)
+        if index_graph_search.get(word.lower(), None):
+            # Append the url and rank to the dict
+            urls_found_rank[word] = [[url, page_rank[url]] for url in index_graph_search[word.lower()]]
+    # Get URLs that appear in more than 1 of the string inputs
     if len(process_input) > 1:
-        # using a counter to keep track of the url values in the dict
+        # using a counter to keep track of the number of urls in the dict
         url_list = [value[0] for k, v in urls_found_rank.items() for value in v]
         counter = Counter(url_list)
-        # Find the matching URLs using the counter
+        # Find the URLs that appear more than once
         matching_urls = [k for k, v in counter.items() if counter[k] > 1]
         if len(matching_urls) > 0:
-            for match_url in matching_urls:
-                urls_found_rank["Overall"].append([match_url, page_rank[match_url]])
+            urls_found_rank[TOTAL_KEY] = [[url, page_rank[url]] for url in matching_urls]
     return urls_found_rank
