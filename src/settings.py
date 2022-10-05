cc0bd88c31a7e409ae048afddf979b4cbf344864
Chris McCourt <chrismccourt66@googlemail.com>
2022-10-05 20:06:01 +0100

    Base settings file created

---
+++ src/settings.py
@@ -0,0 +1,16 @@
+
+
+eihl_base_url = "https://www.eliteleague.co.uk/"
+
+eihl_teams = [
+    "Belfast Giants",
+    "Sheffield Steelers",
+    "Cardiff Devils",
+    "Coventry Blaze",
+    "Dundee Stars",
+    "Glasgow Clan",
+    "Manchester Storm",
+    "Fife Flyers",
+    "Guildford Flames",
+    "Nottingham Panthers"
+]
