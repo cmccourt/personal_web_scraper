a68da6cbd9e07b51f31babf2f531c5d3b228ba2e
Chris McCourt <chrismccourt66@googlemail.com>
2022-10-11 21:16:04 +0100

    Starting implementation of DB connector

---
+++ src/eihl_stats_db.py
@@ -0,0 +1,25 @@
+import psycopg2
+from psycopg2 import _psycopg
+
+from settings.settings import EIHLStatsDBConfig
+
+
+def db_connection() -> _psycopg.connection:
+    db_conn = None
+    try:
+        db_conn = psycopg2.connect(dbname=EIHLStatsDBConfig.db,
+                                   user=EIHLStatsDBConfig.un,
+                                   password=EIHLStatsDBConfig.pw,
+                                   host=EIHLStatsDBConfig.hostname)
+    except psycopg2.DatabaseError:
+        print("ERROR connecting to EIHL Stats Database!!")
+    return db_conn
+
+
+def db_cursor(db_conn: _psycopg.connection) -> _psycopg.cursor:
+    db_cur = None
+    try:
+        db_cur = db_conn.cursor()
+    except psycopg2.Error:
+        print("ERROR creating DB Cursor!")
+    return db_cur
